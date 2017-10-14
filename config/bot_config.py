4cb80fa419ee6652f0d77b6adb44e177115e4f87
anthony <antonsdl@gmail.com>
2017-10-15 02:24:09 +0300

    t-1 added bot config

---
+++ config/bot_config.py
@@ -0,0 +1,10 @@
+"""
+Created by anthony on 15.10.17
+api token gained from @BotFather right from Telegram chat
+get your own for safe local usage
+"""
+
+BOT_NAME = 'Yet Another TimeTracker Bot'
+
+# FIXME
+BOT_API_TOKEN = 'PASTE_YOUR_TOKEN_HERE'
