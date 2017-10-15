e6f585f1e7633e346d8c152301af8891a787501d
anthony <antonsdl@gmail.com>
2017-10-15 03:38:58 +0300

    t-1 removed msg data since we have no data model yet

--- utils/message_parser/message_parser.py
+++ utils/message_parser/message_parser.py
@@ -2,8 +2,7 @@
 Created by anthony on 15.10.17
 message_parser
 """
-from utils.message_parser.message_data import MessageData
 
 
 def parse_message(message):
-    return MessageData()
+    return None
