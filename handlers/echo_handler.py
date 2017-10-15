ca8431ae2af155578a94ba481f048ff97db6a9ea
anthony <antonsdl@gmail.com>
2017-10-15 03:36:21 +0300

    t-1 added logger decorator to echo handler

--- handlers/echo_handler.py
+++ handlers/echo_handler.py
@@ -3,6 +3,7 @@ Created by anthony on 15.10.17
 echo_handler
 """
 from telegram.ext import MessageHandler, Filters
+from utils.handler_utils import add_timestamp, log_duration
 
 
 def echo():
@@ -10,4 +11,10 @@ def echo():
 
 
 def _handle_echo(bot, update):
-    update.message.reply_text(f'You said "{update.message.text}"')
+    update.message.reply_text(text_to_reply(update.message.text))
+
+
+@add_timestamp
+@log_duration
+def text_to_reply(text):
+    return text
