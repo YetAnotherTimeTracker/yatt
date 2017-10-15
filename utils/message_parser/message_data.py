bcd40cce60e83ec5ce6f1376101d4b6349859cfd
anthony <antonsdl@gmail.com>
2017-10-15 03:35:08 +0300

    t-1 added utils

---
+++ utils/message_parser/message_data.py
@@ -0,0 +1,11 @@
+"""
+Created by anthony on 15.10.17
+message_data
+"""
+
+
+class MessageData:
+    def __init__(self):
+        self.data = 'Message payload == task to be created'
+        self.schedule = 'Schedule data from message == when to notify'
+        self.extra = 'Probably some extra data'
---
+++ utils/message_parser/message_parser.py
@@ -0,0 +1,9 @@
+"""
+Created by anthony on 15.10.17
+message_parser
+"""
+from utils.message_parser.message_data import MessageData
+
+
+def parse_message(message):
+    return MessageData()
