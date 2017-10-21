18d08e0879b968082c9f4843ac5a594ea49f0cb1
anthony <antonsdl@gmail.com>
2017-10-22 02:06:54 +0300

    t-5 added models according to ER

--- models/project.py
+++ models/project.py
@@ -2,11 +2,17 @@
 Created by anthony on 19.10.17
 project
 """
-from sqlalchemy import BigInteger, Column, String, DateTime
-import datetime
+from sqlalchemy import BigInteger, Column, String, ForeignKey, Boolean
 from config.db_config import Base
 
 
 class Project(Base):
     __tablename__ = 'projects'
+
+    id              = Column(BigInteger, primary_key=True, autoincrement=True)
+    title           = Column(String)
+    is_active       = Column(Boolean, default=True)
+    # fk
+    next_task_id    = Column(BigInteger, ForeignKey('tasks.id'), nullable=False)
+    user_id         = Column(BigInteger, ForeignKey('users.id'), nullable=False)
 
--- models/task.py
+++ models/task.py
@@ -2,15 +2,21 @@
 Created by anthony on 18.10.17
 task
 """
-from sqlalchemy import BigInteger, Column, String, DateTime
-import datetime
+from sqlalchemy import BigInteger, Column, String, DateTime, SmallInteger, ForeignKey, Boolean
 from config.db_config import Base
+import datetime
 
 
 class Task(Base):
     __tablename__ = 'tasks'
 
-    id = Column(BigInteger, primary_key=True, autoincrement=True)
-    title = Column(String, nullable=False)
-    description = Column(String)
-    create_date = Column(DateTime, default=datetime.datetime.utcnow)
+    id              = Column(BigInteger, primary_key=True, autoincrement=True)
+    title           = Column(String)
+    description     = Column(String, nullable=False)
+    create_date     = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    priority        = Column(SmallInteger, default=1)
+    is_active       = Column(Boolean, default=True)
+    # fk
+    message_id      = Column(BigInteger, ForeignKey('messages.id'), nullable=True)
+    schedule_id     = Column(BigInteger, ForeignKey('schedules.id'), nullable=False)
+    project_id      = Column(BigInteger, ForeignKey('projects.id'), nullable=False)
