a08931c52ddda82e4e453d4dad9f5dfcd4c7fd6e
anthony <antonsdl@gmail.com>
2017-11-04 13:38:50 +0300

    t-9 added todo to wrapper

--- services/queue_service.py
+++ services/queue_service.py
@@ -2,17 +2,12 @@
 Created by anthony on 26.10.17
 queue_service
 """
-from telegram.ext import JobQueue, Job
-# from bot import queue
 
-
+# TODO make wrapper for run_once
 class Notification:
     def __init__(self, bot, chat_id, seconds_till_notification, message):
         # def callback(bot, job):
         #     bot.send_message(chat_id=self.chat_id, text=self.message)
-
-        def callback(bot, job):
-            bot.send_message(chat_id='316956601', text='hallo')
 
         self.bot = bot
         self.chat_id = str(chat_id)
