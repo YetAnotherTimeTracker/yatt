678d1a9c30e3a3b5245b4d3f90122375efcd0c62
anthony <antonsdl@gmail.com>
2017-11-04 13:24:08 +0300

    t-9 added proof of concept notification creator

--- handlers/notification_handler.py
+++ handlers/notification_handler.py
@@ -3,11 +3,6 @@ Created by anthony on 26.10.17
 notification_handler
 """
 from telegram.ext import CommandHandler
-import uuid
-from models.task import Task
-from services import queue_service
-from services.queue_service import Notification
-import bot as bot_module
 
 
 COMMAND = 'q'
@@ -20,33 +15,34 @@ def notify():
 def _handle(bot, update, job_queue):
     bot.send_message(chat_id=update.message.chat_id, text='Received message')
 
-    text = update.message.text
-    args = text.split()
+    args = update.message.text.split()
     seconds_till_notify = args[1]
     notification_message = ' '.join(args[2:])
+    reply_text = None
 
-    reply_text = 'Notification has been created'
-    #
-    # def callback(bot, job):
-    #     bot.send_message(chat_id='316956601', text='hallo')
+    context = {
+        'chat_id': update.message.chat_id,
+        'text': notification_message
+    }
+
     try:
-        # notification = Notification(bot, update.message.chat_id, seconds_till_notify, notification_message)
-        # print('notification has been scheduled')
-        job_queue.run_once(callback_notifier, 5, context=update.message.chat_id)
-        bot.send_message(chat_id=update.message.chat_id, text='Notification has been set')
+        bot.send_message(chat_id=update.message.chat_id, text='Setting up notification')
+
+        seconds_as_float = float(seconds_till_notify)   # do not forget casting to float
+        job_queue.run_once(callback_notifier, seconds_as_float, context=context)
 
     except Exception as e:
-        reply_text = 'There were an error' + str(e)
+        reply_text = 'There were an error: ' + str(e)
+
+    else:
+        reply_text = 'Notification has been set up'
 
     update.message.reply_text(reply_text)
 
 
 def callback_notifier(bot, job):
-    context = job.context   # update.message is passed here via run_once(.. context=...)
-    bot.send_message(chat_id=context, text='Notifying u')
+    context = job.context   # passed here via run_once(.. context=...)
+    chat_id = context['chat_id']
+    message = context['text']
 
-#
-# def callback_timer(bot, update, job_queue):
-#     bot.send_message(chat_id='316956601', text='Setting timer')
-#
-#     job_queue.run_once(callback_notifier, 5, context=update.message.chat_id)
+    bot.send_message(chat_id=chat_id, text=message)
