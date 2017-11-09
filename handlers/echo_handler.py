e9eb239d0e9059b7510edcb9aba0d40e8be6e465
anthony <antonsdl@gmail.com>
2017-11-09 22:52:55 +0300

    fixing issue preventing user creation

--- handlers/echo_handler.py
+++ handlers/echo_handler.py
@@ -12,18 +12,18 @@ def echo():
 
 def _handle(bot, update):
     chat_id = update.message.chat.id
-    user = user_service.find_one_by_chat_id(chat_id)
-    first_name = user.get_first_name()
 
-    new_task = None
     try:
+        user = user_service.find_one_by_id(chat_id)
+        first_name = user.get_first_name()
+
         new_task = task_service.create_task(update)
+
+        if new_task:
+            reply_on_success = f'{first_name}, task with id "{new_task.get_id()}" has been created!'
+            update.message.reply_text(reply_on_success)
 
     except Exception as e:
         reply_on_error = f'Sorry, there were an error: {e}'
         update.message.reply_text(reply_on_error)
         return
-
-    if new_task:
-        reply_on_success = f'{first_name}, task with id "{new_task.get_id()}" has been created!'
-        update.message.reply_text(reply_on_success)
