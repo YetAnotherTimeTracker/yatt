8b2587716a3386cee53c96cd481dca9c3abd48bd
anthony <antonsdl@gmail.com>
2017-11-12 03:43:36 +0300

    t-18 added context to state service

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -3,6 +3,8 @@ Created by anthony on 15.10.17
 start_handler
 """
 from telegram.ext import CommandHandler
+
+from bot import automata
 from services import user_service
 
 
@@ -13,17 +15,41 @@ def start():
     return CommandHandler(COMMAND_START, _handle_start)
 
 
+ENC_NUM = 'encounter_num'
+
 def _handle_start(bot, update):
     reply_msg = 'Hello'
-
+    chat = update.message.chat
     try:
-        chat = update.message.chat
+        if 0 == automata.get_state(chat.id):
+            print('curr state: 0')
+            update.message.reply_text('Haven\'t we met yet?')
+            automata.set_state(chat.id, 1)
+            automata.set_context(chat.id, {ENC_NUM: 0})
+
+        elif 1 == automata.get_state(chat.id):
+            automata.set_state(chat.id, 2)
+            enc_num_value = automata.get_context(chat.id)[ENC_NUM] + 1
+            automata.set_context(chat.id, {ENC_NUM: enc_num_value})
+            update.message.reply_text(f'Oh, I know you. We have me for {enc_num_value} times')
+
+        elif 2 == automata.get_state(chat.id):
+            automata.set_state(chat.id, 3)
+            update.message.reply_text('Hello my dear friend')
+            enc_num_value = automata.get_context(chat.id)[ENC_NUM] + 1
+            automata.set_context(chat.id, {ENC_NUM: enc_num_value})
+
+        else:
+            enc_num_value = automata.get_context(chat.id)[ENC_NUM] + 1
+            automata.set_context(chat.id, {ENC_NUM: enc_num_value})
+            update.message.reply_text(f'Hi! It\'s  the {enc_num_value} we meet :)')
+
         user = user_service.create_or_get_user(chat)
 
         if user:
             reply_msg += ', ' + user.get_first_name()
 
-        update.message.reply_text(reply_msg)
+        #update.message.reply_text(reply_msg)
 
     except Exception as e:
         update.message.reply_text(reply_msg)
--- services/state_service.py
+++ services/state_service.py
@@ -8,6 +8,7 @@ from config.automata_config import TRANSITION_TABLE
 class Automata:
     def __init__(self):
         self.user_to_state = {}
+        self.user_to_context = {}
 
     def get_state(self, chat_id_value):
         chat_id = int(chat_id_value)
@@ -23,6 +24,23 @@ class Automata:
         self.user_to_state[chat_id] = new_state
         return new_state
 
+    def get_context(self, chat_id_value):
+        chat_id = int(chat_id_value)
+        if chat_id in self.user_to_context.keys():
+            return self.user_to_context[chat_id]
+
+        else:
+            self.user_to_context[chat_id] = {}
+            return self.user_to_context[chat_id]
+
+    def set_context(self, chat_id, new_context):
+        chat_id = int(chat_id)
+        self.user_to_context[chat_id] = new_context
+        return new_context
+
     @staticmethod
     def if_can_transit_to(curr_state, command, new_state):
         return new_state == TRANSITION_TABLE[curr_state][command]
+
+    def update_context(self, old_context, new_context):
+        pass
